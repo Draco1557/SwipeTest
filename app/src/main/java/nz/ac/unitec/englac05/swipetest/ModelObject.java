0e22845c779e2d63bb11534e7232bfca01fee36e
Draco1557 <cengland@orcon.net.nz>
2016-11-05 13:58:35 +1300

    Removed one view so there's only two

--- app/src/main/java/nz/ac/unitec/englac05/swipetest/ModelObject.java
+++ app/src/main/java/nz/ac/unitec/englac05/swipetest/ModelObject.java
@@ -6,7 +6,7 @@ package nz.ac.unitec.englac05.swipetest;
 
 public enum ModelObject {
     RED(R.string.red, R.layout.view_red),
-    BLUE(R.string.blue, R.layout.view_blue),
+//    BLUE(R.string.blue, R.layout.view_blue),
     GREEN(R.string.green, R.layout.view_green);
 
     private int mTitleResId;
