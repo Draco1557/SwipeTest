6d104aafdb03722035391baf307560cedaa8f78b
Draco1557 <cengland@orcon.net.nz>
2016-11-05 13:46:40 +1300

    Initial commit

---
+++ app/src/main/java/nz/ac/unitec/englac05/swipetest/ModelObject.java
@@ -0,0 +1,27 @@
+package nz.ac.unitec.englac05.swipetest;
+
+/**
+ * Created by draco on 5/11/2016.
+ */
+
+public enum ModelObject {
+    RED(R.string.red, R.layout.view_red),
+    BLUE(R.string.blue, R.layout.view_blue),
+    GREEN(R.string.green, R.layout.view_green);
+
+    private int mTitleResId;
+    private int mLayoutResId;
+
+    ModelObject(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+}
